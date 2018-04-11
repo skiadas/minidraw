bffbce25a483c9656e783feb54e92923019c2945
henrikbaerbak <hbc@cs.au.dk>
2018-04-11 09:20:35 +0200

    minor

--- minidraw/src/main/java/minidraw/framework/DrawingEditor.java
+++ minidraw/src/main/java/minidraw/framework/DrawingEditor.java
@@ -57,21 +57,6 @@ package minidraw.framework;
  *
  * 1.11: Redefined the image folder for Gradle builds to be 
  * /src/main/resources/minidraw-images to avoid conflicts in resource loading
- * 
- * Copyright 2018 Henrik Baerbak Christensen
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- * 
  */
 
 /*
